adebe96de8d195d3abcd94924e6e7b19de7a1a2d
Oybek Jumaniyozov <jumaniyozov@oybek.com>
2024-01-10 18:53:05 +0500

    Simple modal

--- src/app/components/item-list/item-list.component.ts
+++ src/app/components/item-list/item-list.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-item-list',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './item-list.component.html',
   styleUrl: './item-list.component.less'
 })
 export class ItemListComponent {
+  public modalVisible: boolean = false;
 
+  public showModal(): void {
+    this.modalVisible = true;
+  }
+
+  public hideModal(): void {
+    this.modalVisible = false;
+  }
+
+  public saveChanges(): void {
+    // do something
+    this.modalVisible = false;
+  }
 }
