3b1dc567109a94c2bdf7f6e0fa11d980543f8054
Oybek Jumaniyozov <jumaniyozov@oybek.com>
2024-01-10 19:01:36 +0500

    Component as a modal

--- src/app/components/item-list/item-list.component.ts
+++ src/app/components/item-list/item-list.component.ts
@@ -1,16 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AddItemComponent } from '../add-item/add-item.component';
 
 @Component({
   selector: 'app-item-list',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, AddItemComponent],
   templateUrl: './item-list.component.html',
   styleUrl: './item-list.component.less'
 })
 export class ItemListComponent {
   public modalVisible: boolean = false;
+  public componentModalVisible: boolean = false;
 
   public showModal(): void {
     this.modalVisible = true;
@@ -23,5 +25,18 @@ export class ItemListComponent {
   public saveChanges(): void {
     // do something
     this.modalVisible = false;
+  }
+
+  public showComponentModal(): void {
+    this.componentModalVisible = true;
+  }
+
+  public hideComponentModal(): void {
+    this.componentModalVisible = false;
+  }
+
+  public saveComponentChanges(): void {
+    // do something
+    this.componentModalVisible = false;
   }
 }
