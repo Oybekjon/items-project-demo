0190d31ff65f4d6021a5f3c4b71e1229684d2468
AkobirAbduvahobov <akobirabduvahobov@gmail.com>
2024-01-10 10:25:05 +0500

    changes are done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './components/home/home.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: LayoutComponent,
+        children: [
+            {
+                path: "home",
+                component:HomeComponent
+            },
+            {
+                path: "item-list",
+                component:ItemListComponent
+            },
+            {
+                path: "register",
+                component:RegisterComponent
+            },
+            {
+                path: "login",
+                component:LoginComponent
+            },
+            {
+                path: "",
+                redirectTo: "/home",
+                pathMatch: "full"
+            }
+
+        ]
+    }
+];
