91161c16288c213e2b93fb0f0ac48b0c5c9b5e63
AkobirAbduvahobov <akobirabduvahobov@gmail.com>
2024-01-10 19:31:51 +0500

    qq

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  public value:number = 0;
+
+  public increaseValue() : void {
+    ++this.value;
+    console.log(this.value);
+    this.check();
+  }
+
+  public decreaseValue() : void {
+    --this.value;
+    console.log(this.value);
+    this.check();
+  }
+
+  public check() : void{
+    const v = document.getElementById("decreaseNumber");
+    if(this.value < 0 )
+    {
+      
+      if(v != null)
+      {
+        v.innerHTML = "Stop bitch";
+      }
+    }
+    else
+    {
+      if(v != null)
+      {
+        v.innerHTML = "Decrease";
+      }
+    }
+  }
+
 }
